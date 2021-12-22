087c811f939a262ecf9d28f3d3b1409c7b5bf6d4
rajkumari <213279@nttdata.com>
2021-12-22 19:33:38 +0530

    Add project files.

---
+++ Controllers/rabbitController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ_CURD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_CURD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class rabbitController :   ControllerBase
+    {
+        private IRabbitManager _manager;
+
+        public rabbitController(IRabbitManager manager)
+        {
+            _manager = manager;
+        }
+        // GET api/values  
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {            
+
+            // if above operation succeed, publish a message to RabbitMQ  
+
+            var num = new System.Random().Next(9000);
+
+            // publish message  
+            _manager.Publish(new
+            {
+                field1 = $"Hello-{num}",
+                field2 = $"rabbit-{num}"
+            }, "demo.exchange.topic.dotnetcore", "topic", "*.queue.durable.dotnetcore.#");
+
+            return new string[] { "value1", "value2" };
+        }
+
+
+    }
+}
